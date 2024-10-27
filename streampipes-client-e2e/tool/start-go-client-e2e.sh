8a210053f9040ad9af908afec06780db23bfe581
luoluoyuyu <zhenyu@apache.org>
2024-10-27 22:25:52 +0800

    test

--- streampipes-client-e2e/tool/start-go-client-e2e.sh
+++ streampipes-client-e2e/tool/start-go-client-e2e.sh
@@ -34,13 +34,39 @@ loginRequestBody='{
 }'
 
 # Login and get accessToken
-response=$(curl -s -X POST "http://$HOST:$PORT$LOGIN_URL" \
+#response=$(curl -s -X POST "http://$HOST:$PORT$LOGIN_URL" \
+#   -H "Content-Type: application/json" \
+#   -d "$loginRequestBody")
+#if [ $? -ne 0 ]; then
+#    echo "Error: Login request failed"
+#    exit 1
+#fi
+
+# Login and get accessToken
+curl_output="curl_output.txt"
+curl_error="curl_error.txt"
+
+response=$(curl -s -o "$curl_output" -e "$curl_error" -w "%{http_code}" -X POST "http://$HOST:$PORT$LOGIN_URL" \
    -H "Content-Type: application/json" \
-   -d "$loginRequestBody")
+   -d "$loginRequestBody" 2> "$curl_error")
+
 if [ $? -ne 0 ]; then
     echo "Error: Login request failed"
+    # Print the error details from curl_error
+    echo "Error details:"
+    cat "$curl_error"
     exit 1
+else
+    echo "Login request successful"
+    # Print the response details from curl_output
+    echo "Response details:"
+    cat "$curl_output"
+    # You can also print the HTTP status code
+    echo "HTTP Status Code: $response"
 fi
+
+# Remove the temporary files if not needed anymore
+rm "$curl_output" "$curl_error"
 
 accessToken=$(echo "$response" | sed -n 's/.*"accessToken":"\([^"]*\)".*/\1/p')
 
